4ab0ba612ea9a59f7bd447a8d63623163114ec7c
Augusto Saboia <augustosaboiaestudos@gmail.com>
2024-11-20 01:01:17 -0300

    Adicionar arquivos de projeto.

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+
+                string nomeCaneta = txt_Caneta.Text;
+                double precoCaneta = Convert.ToDouble(txt_Preco.Text);
+                string msg = "";
+                if (precoCaneta < 5)
+                {
+                    msg = "Caneta BARATA";
+                }
+                else
+                {
+                    msg = "Caneta Cara";
+                }
+                DisplayAlert("Adicionado", msg, "Ok");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+        } // Fecha método
+
+    }// Fecha Class
+
+}// Fecha namespace
